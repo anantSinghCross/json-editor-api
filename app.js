7641d1ab9917e0ffb417b06415871203bdd8094f
Anant Singh <38243636+anantSinghCross@users.noreply.github.com>
2024-01-18 22:48:15 +0530

    Added POST route

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const fs = require('fs');
-const fsp = fs.promises;
 
 const app = express();
+
+app.use(express.json());
 
 app.get('/api/', (req, res, next) => {
     fs.readFile('C:/Users/anant/OneDrive/Desktop/sample_json_files/file.json', (err, data) => {
@@ -10,5 +11,15 @@ app.get('/api/', (req, res, next) => {
         res.send(json);
     })
 });
+
+app.post('/api/', (req, res, next) => {
+    console.log(JSON.stringify(req.body));
+    fs.writeFile('C:/Users/anant/OneDrive/Desktop/sample_json_files/file.json', JSON.stringify(req.body), (err) => {
+        if(!err){
+            return res.status(200).json({msg: 'File written successfully.'});
+        }
+        res.send(500).json({msg: 'Error saving the data. Please try re-saving.'});
+    })
+})
 
 app.listen(3000);
