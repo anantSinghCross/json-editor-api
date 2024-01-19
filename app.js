b7a03e63890c883f880ad610e90aad5e6d0a8edf
Anant Singh <38243636+anantSinghCross@users.noreply.github.com>
2024-01-19 22:09:19 +0530

    Few tweaks

--- app.js
+++ app.js
@@ -13,12 +13,11 @@ app.get('/api/', (req, res, next) => {
 });
 
 app.post('/api/', (req, res, next) => {
-    console.log(JSON.stringify(req.body));
     fs.writeFile('C:/Users/anant/OneDrive/Desktop/sample_json_files/file.json', JSON.stringify(req.body), (err) => {
         if(!err){
-            return res.status(200).json({msg: 'File written successfully.'});
+            return res.status(200).json({ success: true, msg: 'File written successfully.'});
         }
-        res.send(500).json({msg: 'Error saving the data. Please try re-saving.'});
+        res.send(500).json({success: false, msg: 'Error saving the data. Please try re-saving.'});
     })
 })
 
